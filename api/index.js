280865e404553900e127de2541bbe40a4ecadaa1
KALAISELVAN CK <posttokalai@gmail.com>
2024-11-14 22:49:55 +0530

    change index.js

--- api/index.js
+++ api/index.js
@@ -387,8 +387,4 @@ app.delete(`/profiles/:profile_id`, async (req, res) => {
   }
 });
 
-// Server setup
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+export default app;
