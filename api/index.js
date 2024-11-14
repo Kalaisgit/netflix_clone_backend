a1055062697018388e0c947cf31bd10de43b3fbc
KALAISELVAN CK <posttokalai@gmail.com>
2024-11-14 19:17:24 +0530

    change index.js

--- api/index.js
+++ api/index.js
@@ -5,8 +5,6 @@ import dotenv from "dotenv";
 import "../config/passportConfig.js"; // Passport config for Google strategy
 import cors from "cors"; // Import the cors package
 import { createClient } from "@supabase/supabase-js"; // Import the Supabase client
-import pkg from "@vercel/node";
-const { createRequestHandler } = pkg;
 
 dotenv.config();
 
@@ -218,4 +216,4 @@ app.get(`/favorites`, async (req, res) => {
   }
 });
 
-export default createRequestHandler(app);
+export default app;
